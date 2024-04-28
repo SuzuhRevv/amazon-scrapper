95a57809461a7e501f6cd352d81aeb900db17c48
SuzuhRevv <joaopk04@hotmail.com>
2024-04-27 21:06:50 -0300

    Added the GET method to receive the data based on the keyword and also used axios to get that data and convert into JSON

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const jsdom = require('jsdom')
 const app = express()
 const port = 3000
 
+app.get('/api/scrape', async (req, res) => {
+    const keyword = req.query
+    if (!keyword) throw new Error('Keyword is needed!')
+    const amazonURL = `https://www.amazon.com/s?k=${keyword}`
+    const resp = axios.get(amazonURL)
+    res.send('Hello')
+})
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
